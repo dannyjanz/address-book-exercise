baaa6c2adf48d74ce70d84932749a984a2e4e58b
dannyjanz <github666>
2017-11-02 00:01:05 -0400

    adding function to find oldest Person in a sequence of Persons and the corresponding unit test

--- src/main/scala/ExtractPerson.scala
+++ src/main/scala/ExtractPerson.scala
@@ -3,20 +3,17 @@ import scala.util.{Failure, Success, Try}
 
 object ExtractPerson {
 
-  def apply(person: String): Try[Person] = {
+  def apply(person: String): Try[Person] = person split ',' match {
+    case Array(nameString, genderString, birthdayString) =>
+      for {
+        (firstName, lastName) <- extractName(nameString.trim)
+        birthdayDate <- ExtractDate(birthdayString.trim)
+        gender <- ExtractGender(genderString.trim)
+      } yield {
+        Person(firstName, lastName, gender, birthdayDate)
+      }
 
-    person split ',' match {
-      case Array(nameString, genderString, birthdayString) =>
-        for {
-          (firstName, lastName) <- extractName(nameString.trim)
-          birthdayDate <- ExtractDate(birthdayString.trim)
-          gender <- ExtractGender(genderString.trim)
-        } yield {
-          Person(firstName, lastName, gender, birthdayDate)
-        }
-
-      case _ => Failure(new IllegalArgumentException(s"'$person' does not match the expected format"))
-    }
+    case _ => Failure(new IllegalArgumentException(s"'$person' does not match the expected format"))
   }
 
   private def extractName(name: String) = name split ' ' match {
